4f8f5072ffe800a59ce0299102a3325910c01936
Roland Michelberger <roland.michelberger@ti8m.ch>
2016-03-11 12:21:53 +0100

    - added new EchnageRates model (because of the deprecated web service)

---
+++ Shopping/ExchangeRates.swift
@@ -0,0 +1,47 @@
+//
+//  ExchangeRates.swift
+//  Shopping
+//
+//  Created by Roland Michelberger on 11/03/16.
+//  Copyright © 2016 Roland Michelberger. All rights reserved.
+//
+
+import Foundation
+
+struct ExchangeRates {
+
+    let source: Currency
+    let quotes: [ExchangeRate]
+    
+    init(source: Currency, quotes: [ExchangeRate]) {
+        self.source = source
+        self.quotes = quotes
+    }
+    
+    init? (dict: [String: AnyObject]) {
+        
+        // if source not found
+        guard let sourceString = dict["source"] as? String else {
+            return nil
+        }
+        
+        self.source = Currency(string: sourceString)
+        
+        // if quotes not found
+        guard let quotesDict = dict["quotes"] as? [String: AnyObject] else {
+            return nil
+        }
+        
+        var quotes = [ExchangeRate]()
+        
+        for key in quotesDict.keys {
+            let currency = Currency(string: key.stringByReplacingOccurrencesOfString(source.rawValue, withString: ""))
+            if let rate = quotesDict[key]?.doubleValue {
+                let exchangeRate = ExchangeRate(from: source, to: currency, rate: rate)
+                quotes.append(exchangeRate)
+            }
+        }
+        
+        self.quotes = quotes
+    }
+}
