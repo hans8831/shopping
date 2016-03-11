5a58b0d9b6555310cc4eda401c4231d906415080
Roland Michelberger <roland.michelberger@ti8m.ch>
2016-03-11 08:08:32 +0100

    - added models, network and price converter util empty classes

---
+++ Shopping/Currency.swift
@@ -0,0 +1,13 @@
+//
+//  Currency.swift
+//  Shopping
+//
+//  Created by Roland Michelberger on 10/03/16.
+//  Copyright © 2016 Roland Michelberger. All rights reserved.
+//
+
+import UIKit
+
+class Currency: NSObject {
+
+}
---
+++ Shopping/Server.swift
@@ -0,0 +1,13 @@
+//
+//  Server.swift
+//  Shopping
+//
+//  Created by Roland Michelberger on 10/03/16.
+//  Copyright © 2016 Roland Michelberger. All rights reserved.
+//
+
+import UIKit
+
+class Server: NSObject {
+
+}
